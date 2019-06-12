34e8d50101a7e4d3ff63d82d6ca48e05bb2d0da0
Bora Guven <guvenbe@gmail.com>
2019-06-12 00:01:33 -0400

    added assertThat

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
 
@@ -14,17 +22,82 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test proper biew name is returned for index page")
     @Test
     void index() {
         assertEquals("index",controller.index());
         assertEquals("index",controller.index(),"wrong view returned");
         assertEquals("index",controller.index(),() ->"Another wrong view returned");
+        assertThat(controller.index().equals("index"));
     }
 
     @Test
+    @DisplayName("Test Exception")
     void oupsHandler() {
-        assertTrue("notimplemented".equals(controller.oupsHandler()),() -> "This is some expensive" +
-                "Message to build" +
-                "for my test");
+        assertThrows(ValueNotFoundException.class, () -> {
+            controller.oupsHandler();
+        });
+        
+//        assertTrue("notimplemented".equals(controller.oupsHandler()),() -> "This is some expensive" +
+//                "Message to build" +
+//                "for my test");
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOut() {
+        assertTimeout(Duration.ofMillis(100),
+                () -> {Thread.sleep(2000);
+                    System.out.println("I got here");
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOutPrempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100),
+                () -> {Thread.sleep(2000);
+                    System.out.println("I got here xxxxx");
+        });
+    }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equals(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionTrueAssumotionIsTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS() {
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11() {
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "guvenbe")
+    @Test
+    void testIfUserJT() {
+    }
+
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "fredBBBBBB")
+    @Test
+    void testIfUserFred() {
     }
 }
---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1l, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1231231234");
+
+        assertAll("Properties Test",
+            () -> assertAll("Person Properties",
+                    () -> assertEquals("Joe", owner.getFirstName(), "Failed on first name"),
+                    () -> assertEquals("Buck", owner.getLastName(), "Failed on last name")),
+            ()->  assertAll("Owner Properties",
+                    () -> assertEquals("Key West", owner.getCity(), "Failed on city"),
+                    () -> assertEquals("1231231234", owner.getTelephone(), "Failed on Telephone")
+            ));
+
+        assertThat(owner.getCity(), is("Key West"));
+
+    }
+
+
+
+}
